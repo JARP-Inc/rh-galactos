c7b4d431bc13f8e3dd72ac5d5fd2f937bba7ca1d
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-02-04 22:43:53 +0000

    made stuff look pretty

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { ModalProvider } from "@hooks/useModal";
 import type { FC } from "react";
 import AuthProfile from "./AuthProfile";
 
@@ -5,7 +6,7 @@ interface props {}
 
 const Header: FC<props> = () => {
   return (
-    <>
+    <ModalProvider>
       <div className="navbar bg-primary sticky top-0 z-10 px-16 shadow-lg">
         <div className="flex-1 items-center gap-10">
           <img className="my-6 h-16" src={"/Logo.svg"} />
@@ -26,7 +27,7 @@ const Header: FC<props> = () => {
           <AuthProfile />
         </div>
       </div>
-    </>
+    </ModalProvider>
   );
 };
 

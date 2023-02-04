3ee33ef458029b6b3e2c38b91830610aa64542bc
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-02-04 14:35:45 +0000

    updated fakes with picture of sir humphrey

--- frontend/src/hooks/getAuthor.ts
+++ frontend/src/hooks/getAuthor.ts
@@ -3,7 +3,7 @@ function getAuthor(name: string): Author {
     name: name,
     description:
       "Duis fringilla erat sit amet nisi porta, vitae lacinia turpis tempor. Nam viverra pharetra volutpat. Vestibulum sit amet mattis urna, eget faucibus tortor. Vivamus et lorem id ex eleifend sollicitudin at in felis. Phasellus vel lacus eget elit finibus finibus eget ac nulla. In lacus quam, rhoncus non ipsum feugiat, imperdiet dignissim nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque pellentesque fermentum luctus. Sed scelerisque molestie iaculis. Aenean commodo massa at sapien commodo, ut laoreet turpis consequat. Nulla molestie odio rutrum commodo auctor.",
-    picture_url: "",
+    picture_url: "/sir_humphrey.jpg",
   };
 }
 

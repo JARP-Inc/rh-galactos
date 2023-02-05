0e7b94aa757cec47f3b8e4e5d20d14e15e4ba884
Ashleen <2552805d@student.gla.ac.uk>
2023-02-05 12:18:35 +0000

    fixed port so it is now parsed as an integer

--- backend/main.py
+++ backend/main.py
@@ -5,4 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv('PORT'), reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=int(
+        os.getenv('PORT')), reload=True)

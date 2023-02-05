1e20143b9f40774734c9302e0e6aac1a73c7c862
Ashleen <2552805d@student.gla.ac.uk>
2023-02-05 12:11:29 +0000

    UPDATED port

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv('PORT'), reload=True)
